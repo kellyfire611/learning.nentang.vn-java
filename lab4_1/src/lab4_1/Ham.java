08930ce2b4d828807a7c355dd10506d157fffe7d
kellyfire <kellyfire611@gmail.com>
2024-09-13 04:48:42 +0700

    update lab2, lab3, lab4.

---
+++ lab4_1/src/lab4_1/Ham.java
@@ -0,0 +1,14 @@
+package lab4_1;
+
+public class Ham {
+	public float tinh_trung_binh_cong(float a, float b, float c) {
+		float tong = a + b + c;
+		float ketqua = tong / 3;
+		return ketqua;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
